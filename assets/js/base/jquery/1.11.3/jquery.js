7a9a5a6bf71b085b24e3e1a0dead7c8c470f0851
Nuintun <nuintun@qq.com>
2015-05-21 09:53:31 +0800

    add source

--- assets/js/base/jquery/1.11.3/jquery.js
+++ assets/js/base/jquery/1.11.3/jquery.js
@@ -13,7 +13,6 @@
  */
 
 function jQuery(window, noGlobal){
-
   // Can't do this because several apps including ASP.NET trace
   // the stack via arguments.caller.callee and Firefox dies if
   // you try to trace through "use strict" call chains. (#13335)
