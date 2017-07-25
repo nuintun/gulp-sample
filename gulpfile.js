0e680da7d4f6058da6f1cabc3b2406d5ae56fb07
nuintun <nuintun@qq.com>
2017-07-25 10:13:42 +0800

    adjust code style

--- gulpfile.js
+++ gulpfile.js
@@ -216,13 +216,13 @@ function dateFormat(date, format) {
   format = format || 'yyyy-MM-dd hh:mm:ss';
 
   var map = {
-    'M': date.getMonth() + 1, //月份
-    'd': date.getDate(), //日
-    'h': date.getHours(), //小时
-    'm': date.getMinutes(), //分
-    's': date.getSeconds(), //秒
-    'q': Math.floor((date.getMonth() + 3) / 3), //季度
-    'S': date.getMilliseconds() //毫秒
+    'M': date.getMonth() + 1, // 月份
+    'd': date.getDate(), // 日
+    'h': date.getHours(), // 小时
+    'm': date.getMinutes(), // 分
+    's': date.getSeconds(), // 秒
+    'q': Math.floor((date.getMonth() + 3) / 3), // 季度
+    'S': date.getMilliseconds() // 毫秒
   };
 
   format = format.replace(/([yMdhmsqS])+/g, function(all, t) {
